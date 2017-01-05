2d1559d842e617aa73c11b8697fee89b8e52e38e
Administrator <Administrator@SC-201611031421>
2017-01-05 17:37:47 +0800

    添加项目文件。

---
+++ MvcMovie/Data/Migrations/20170105091951_a.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Data.Migrations
+{
+    public partial class a : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                maxLength: 60,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                maxLength: 5,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                maxLength: 30,
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                nullable: true);
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)] //手工高亮
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)] //手工高亮
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), Required, StringLength(30)] //手工高亮
+        public string Genre { get; set; }
+
+        [Range(1, 100), DataType(DataType.Currency)] //手工高亮
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), StringLength(5)] //手工高亮
+        public string Rating { get; set; }
+    }
+}
